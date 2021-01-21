5970452485246fe800ad75389a788100bbe1b883
Jean Dessane <jean@habx.fr>
2021-01-21 17:18:36 +0100

    Allow to move using 2 fingers with pinch event

--- src/controls/HammerGestures.js
+++ src/controls/HammerGestures.js
@@ -52,7 +52,7 @@ HammerGestures.prototype.get = function(element, type) {
 
 
 HammerGestures.prototype._createManager = function(element, type) {
-  var manager = new Hammer.Manager(element);
+  var manager = new Hammer.Manager(element, { dragMaxTouches: 2 });
 
   // Managers are created with different parameters for different pointer
   // types.
